6a02e1ace540baff09063eb3c6a4bad5bbeefee0
Wojciech Brüggemann <wojtek77@o2.pl>
2015-10-02 08:41:18 +0200

    dodanie Hello World

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,8 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $this->_helper->FlashMessenger->addMessage('jakis error');
-        v('hello world');
+        
     }
 }
 
